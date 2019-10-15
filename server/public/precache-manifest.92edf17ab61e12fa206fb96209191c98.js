8515d0c028e652bb1757438d2bb1e64ac84ab40c
Javier Garcia <fominayajg@gmail.com>
2019-10-15 11:51:23 +0200

    g

---
+++ server/public/precache-manifest.92edf17ab61e12fa206fb96209191c98.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "114fec2f389d97601ff9",
+    "url": "/static/js/main.114fec2f.chunk.js"
+  },
+  {
+    "revision": "98309860a40b7a336982",
+    "url": "/static/js/1.98309860.chunk.js"
+  },
+  {
+    "revision": "114fec2f389d97601ff9",
+    "url": "/static/css/main.f3749d6f.chunk.css"
+  },
+  {
+    "revision": "5f43d5ed929f57c6dcce8530f7b75419",
+    "url": "/index.html"
+  }
+];
---
+++ server/public/precache-manifest.a34b3dacb5ce0c8a7a31fb8dca804bb0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "1006e6f7271acd787440",
+    "url": "/static/js/main.1006e6f7.chunk.js"
+  },
+  {
+    "revision": "98309860a40b7a336982",
+    "url": "/static/js/1.98309860.chunk.js"
+  },
+  {
+    "revision": "1006e6f7271acd787440",
+    "url": "/static/css/main.f3749d6f.chunk.css"
+  },
+  {
+    "revision": "75e32485e602788a48112462a43f9bef",
+    "url": "/index.html"
+  }
+];
